711bec9a9e8125d94014f80715246498749893fa
Edgar Britez <edgarfbritez@gmail.com>
2021-09-26 06:50:57 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+const baseUrl = "https://superheroapi.com/api/1621075358241982";
 
 function App() {
+  const [post, setPost] = React.useState(null);
+  React.useEffect(() => {
+    axios.get(`${baseUrl}`).then((res) => console.log("res", res));
+  }, []);
+
+  function createPost() {
+    axios
+      .post(baseUrl, {
+        title: "Hello World!",
+        body: "this is a new post.",
+        pic: "this will be an image",
+      })
+      .then((res) => setPost(res.data));
+  }
+
+  if (!post) return "No post!";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <p>{post.pic}</p>
+      <button onClick={createPost}>Create Post</button> */}
     </div>
   );
 }
