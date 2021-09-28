80f7a31538aa1b658eca9622340752ad31e7b124
Edgar Britez <edgarfbritez@gmail.com>
2021-09-27 22:19:23 -0300

    deploy to vercel to try axios

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import axios from "axios";
 import LogIn from "./components/LogIn/LogIn";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home";
 import Heroes from "./pages/Heroes";
 
-const token = "1621075358241982";
+const TOKEN_API = "1621075358241982";
 const accessToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJjaGFsbGVuZ2VAYWxrZW15Lm9yZyIsImlhdCI6MTUxNjIzOTAyMn0.ilhFPrG0y7olRHifbjvcMOlH7q2YwlegT0f4aSbryBE";
-const baseUrl = `https://superheroapi.com/api/1621075358241982/search/batman`;
+const baseUrl = "https://superheroapi.com/api/1621075358241982/";
 
 function App() {
+  React.useEffect(() => {
+    axios
+      .get(`${baseUrl}45`)
+      .then((res) => console.log(res))
+      .catch((error) => console.log(error.response));
+  }, []);
   function logOutHandler() {
     localStorage.removeItem("userToken");
     setUserToken("");
